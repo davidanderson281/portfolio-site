46000c284462553a280a523d6c16bd36ff83eafe
davidanderson281 <david.anderson281@yahoo.co.uk>
2021-03-06 14:43:45 +0000

    First commit

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import Content from '../components/Content';
+
+function AboutPage(props) {
+    return(
+        <div>
+            <Hero title={props.title} />
+            <Content>
+                This is some content abiut me!
+            </Content>
+        </div>
+    );
+}
+
+export default AboutPage;
